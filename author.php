598cd7620c12ce2d562fda7465df9ef6940d19b7
oka@mauderewrite.com <oka@mauderewrite.com>
2018-12-03 17:45:58 -0500

    make author template generic

--- author.php
+++ author.php
@@ -1,26 +1,21 @@
 <?php
 /**
  * Template: Author Archive
- * Description: Wordpress template for author archive page
+ * Description: Wordpress template for author archive page.
  *
  */
     get_header();
 
 
-    $maxPages = $wp_query->max_num_pages;
-    $count = $wp_query->post_count;
-    $author = get_the_author();
-    $authorID = get_the_author_meta('user_nicename');
+    $maxPages = $wp_query->max_num_pages; //Find the max number of pages for the query, necessary for "Load More"
+    $authorID = get_the_author_meta('ID'); //Find the author's ID
 
-    echo Utils::render_template("inc/templates/author-feed.php", array(
-        "title"         => $author,
+    echo Utils::render_template("components/component_author-feed.php", array(
         "maxPages"      => $maxPages,
         "authorID"      => $authorID,
-        "query"         => $wp_query,
-        "postType"      => 'post',
-        "count"         => $count,
+        "query"         => $wp_query, // $wp_query is the default WordPress query. It uses the posts per page set in wp-admin.
+        "postType"      => 'post', //Set post type to be displayed for the author
     ));
 
     get_footer();
 ?>
-
