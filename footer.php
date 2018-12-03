35cef7dc81ba2acad1835e12b6690ca9dfbb0d98
oka@mauderewrite.com <oka@mauderewrite.com>
2018-12-03 17:49:26 -0500

    clean up footer

--- footer.php
+++ footer.php
@@ -1,18 +1,10 @@
-</main>
-</div>
-<?= get_global_option('hubspot_global_js_includes', 'options');?>
-<footer class="page-footer component-theme-black row-text-white dont-fade-me-in">
+	</main><!-- Close site <main> -->
+</div> <!-- Close content-container div -->
+<footer class="page-footer dont-fade-me-in">
 	<div class="footer-wrapper">
-		<?php include 'inc/templates/footer-inner.php'; ?>
+		<?php include 'componers/component_footer-inner.php'; ?>
 	</div>
 </footer>
-<div class="search-popup dont-fade-me-in">
-	<?php 
-        echo Utils::render_template("inc/templates/search-form.php", array(
-            "searchTerm" => ''
-        ));
-    ?>
-</div>
 
 <?php wp_footer(); ?>
 </body>
