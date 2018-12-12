fccfbaeb99314297ef755c5da0b1c03c75ccf9dd
Sarah Quigley <sarah@sarahquigley.net>
2018-12-12 15:55:46 -0500

    WIP: work on color customization

---
+++ __GET_STARTED_HERE/boilerplate-child/config/global-variables/colors.php
@@ -0,0 +1,18 @@
+<?php
+
+$colors = array(
+  array(
+    'default'     => '#0000ff',
+    'description' => 'Add a color to use within the Gutenberg editor color palette.',
+    'label'       => 'Primary Color',
+    'slug'        => 'primary-color',
+  ),
+  array(
+    'default'     => '#00ffff',
+    'description' => 'Add a color to use within the Gutenberg editor color palette.',
+    'label'       => 'Secondary Color',
+    'slug'        => 'secondary-color',
+  ),
+);
+
+define('ATTCK_COLORS', $colors);
