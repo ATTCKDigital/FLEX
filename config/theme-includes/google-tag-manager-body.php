5333b76b10165982e9e6e52e71e2252768d73504
oka@mauderewrite.com <oka@mauderewrite.com>
2018-12-17 15:56:38 -0500

    make GTM conditional on having an ID value

--- config/theme-includes/google-tag-manager-body.php
+++ config/theme-includes/google-tag-manager-body.php
@@ -1,4 +1,8 @@
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=<?= get_field('gtm_ID', 'options');?>"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
+<?php $GTM_ID = get_field('gtm_ID', 'options');?>
+
+<?php if($GTM_ID) { ?>
+	<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=<?= get_field('gtm_ID', 'options');?>"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
+<?php } ?>
