547ce83a2a5a78070570e23e014e79bc538305e2
Sarah Quigley <sarah@sarahquigley.net>
2019-03-05 14:26:33 -0500

    chore: clean up flex-layout-system php config

--- config/theme-configs/flex-layout-system.php
+++ config/theme-configs/flex-layout-system.php
@@ -24,71 +24,39 @@ namespace FLEX_LAYOUT_SYSTEM;
 //  Exit if accessed directly.
 defined('ABSPATH') || exit;
 
-/**
- * Gets this plugin's absolute directory path.
- *
- * @since  2.1.0
- * @ignore
- * @access private
- *
- * @return string
- */
-function _get_plugin_directory() {
-	return THEME_DIR;
-}
-
-/**
- * Gets this plugin's URL.
- *
- * @since  2.1.0
- * @ignore
- * @access private
- *
- * @return string
- */
-function _get_plugin_url() {
-	static $plugin_url;
-
-	if ( empty( $plugin_url ) ) {
-		$plugin_url = plugins_url( null, __FILE__ );
-	}
-
-	return $plugin_url;
-}
-
 // Components
-include THEME_DIR . '/gb-components/gb-component_background-options/attributes.php';
-include THEME_DIR . '/gb-components/gb-component_background-options/classes.php';
-include THEME_DIR . '/gb-components/gb-component_background-options/inline-styles.php';
-include THEME_DIR . '/gb-components/gb-component_background-options/video.php';
+include THEME_DIR . '/gutenberg/components/gb-component_background-options/attributes.php';
+include THEME_DIR . '/gutenberg/components/gb-component_background-options/classes.php';
+include THEME_DIR . '/gutenberg/components/component_background-options/inline-styles.php';
+include THEME_DIR . '/gutenberg/components/gb-component_background-options/video.php';
 
-include THEME_DIR . '/gb-components/gb-component_columns/attributes.php';
-include THEME_DIR . '/gb-components/gb-component_columns/classes.php';
+include THEME_DIR . '/gutenberg/components/gb-component_columns/attributes.php';
+include THEME_DIR . '/gutenberg/components/gb-component_columns/classes.php';
 
-include THEME_DIR . '/gb-components/gb-component_logo-color/attributes.php';
-include THEME_DIR . '/gb-components/gb-component_logo-color/data-attributes.php';
+include THEME_DIR . '/gutenberg/components/gb-component_logo-color/attributes.php';
+include THEME_DIR . '/gutenberg/components/gb-component_logo-color/data-attributes.php';
 
-include THEME_DIR . '/gb-components/gb-component_margin/attributes.php';
-include THEME_DIR . '/gb-components/gb-component_margin/classes.php';
+include THEME_DIR . '/gutenberg/components/gb-component_margin/attributes.php';
+include THEME_DIR . '/gutenberg/components/gb-component_margin/classes.php';
 
-include THEME_DIR . '/gb-components/gb-component_padding/attributes.php';
-include THEME_DIR . '/gb-components/gb-component_padding/classes.php';
+include THEME_DIR . '/gutenberg/components/gb-component_padding/attributes.php';
+include THEME_DIR . '/gutenberg/components/gb-component_padding/classes.php';
 
-include THEME_DIR . '/gb-components/gb-component_row-height/attributes.php';
-include THEME_DIR . '/gb-components/gb-component_row-height/classes.php';
+include THEME_DIR . '/gutenberg/components/gb-component_row-height/attributes.php';
+include THEME_DIR . '/gutenberg/components/gb-component_row-height/classes.php';
 
-include THEME_DIR . '/gb-components/gb-component_text-colors/attributes.php';
-include THEME_DIR . '/gb-components/gb-component_text-colors/classes.php';
+include THEME_DIR . '/gutenberg/components/gb-component_text-colors/attributes.php';
+include THEME_DIR . '/gutenberg/components/gb-component_text-colors/classes.php';
 
-include THEME_DIR . '/gb-components/gb-component_scroller/attributes.php';
-include THEME_DIR . '/gb-components/gb-component_scroller/scroller-output.php';
+include THEME_DIR . '/gutenberg/components/gb-component_scroller/attributes.php';
+include THEME_DIR . '/gutenberg/components/gb-component_scroller/scroller-output.php';
 
 // Dynamic Blocks
-include THEME_DIR . '/blocks/block_button/button.php';
-include THEME_DIR . '/blocks/block_column/column.php';
-include THEME_DIR . '/blocks/block_heading/heading.php';
-include THEME_DIR . '/blocks/block_image/image.php';
-include THEME_DIR . '/blocks/block_quote/quote.php';
-include THEME_DIR . '/blocks/block_row/row.php';
-include THEME_DIR . '/blocks/block_source/source.php';
-include THEME_DIR . '/blocks/block_text/text.php';
+include THEME_DIR . '/gutenberg/blocks/block_button/button.php';
+include THEME_DIR . '/gutenberg/blocks/block_column/column.php';
+include THEME_DIR . '/gutenberg/blocks/block_heading/heading.php';
+include THEME_DIR . '/gutenberg/blocks/block_image/image.php';
+include THEME_DIR . '/gutenberg/blocks/block_quote/quote.php';
+include THEME_DIR . '/gutenberg/blocks/block_row/row.php';
+include THEME_DIR . '/gutenberg/blocks/block_source/source.php';
+include THEME_DIR . '/gutenberg/blocks/block_text/text.php';
