4d97be7ec79c59b92d92a0e67eba8e3264029c56
oka@mauderewrite.com <oka@mauderewrite.com>
2019-03-07 16:48:22 -0500

    make some notes about acf sync, add options page to parent theme, add year shortcode for use in admin

--- config/acf-configs/acf-sync.php
+++ config/acf-configs/acf-sync.php
@@ -1,37 +1,42 @@
 <?php
 /*** ACF Local JSON and Sync ***/
-add_filter('acf/settings/save_json', 'flexls_acf_json_save_point');
+// ACF syncs fields based on field group json files. 
+// ACF blocks will save their json to the block folder when using ACF Local JSON Manager Plugin.  Prior to creating new ACF block fields, select the correct block from the "Local JSON" menu in the admin toolbar. This will save the file to the correct location.
 
+// https://www.advancedcustomfields.com/resources/local-json/
+// https://www.advancedcustomfields.com/resources/synchronized-json/
+
+
+/** ACF Save Point **/
+add_filter('acf/settings/save_json', 'flexls_acf_json_save_point');
 function flexls_acf_json_save_point( $path ) {
-	// update path
+	// Update to custom path
 	$path = THEME_DIR.'/config/acf-configs/acf-json';
 	
 	// return
 	return $path;
 }
 
+/** ACF Load Point **/
+// Any changes made to a parent theme json file from the child theme will save to the parent theme save point.
+// https://support.advancedcustomfields.com/forums/topic/acf-json-fields-not-loading-from-parent-theme/
 add_filter('acf/settings/load_json', 'flexls_acf_json_load_point');
-
 function flexls_acf_json_load_point( $paths ) {
-	// append path
+	// Append custom paths
 	$paths = array(THEME_DIR.'/config/acf-configs/acf-json');
 
-
-	if(is_child_theme())
-	{
+	if(is_child_theme()) {
+		//If we are in the child theme, check for child theme specific json files
 		$paths[] = CHILD_THEME_DIR.'/config/acf-configs/acf-json';
 	}
 
 	// return
 	return $paths;
-
 }
 
 
 
-/**
- * Update ACF settings.
- */
+/** Update ACF settings. **/
 function flexls_acf_init() {
 	// Save field changes to database (set to to 'false'). Only for Dev environment
 	acf_update_setting('json', true);
@@ -40,15 +45,15 @@ function flexls_acf_init() {
 add_action('acf/init', 'flexls_acf_init');
 
 
-/*** Sync Registered Blocks ***/
+/** Sync Registered Blocks **/
 
 add_filter('aljm_save_json', function($folders) {
-	//get the list of registered blocks and sync the json for each one into it's component folder
+	// Get the list of registered blocks and sync the json for each one into it's component folder
 	$registerBlocks = FLEXLS_REGISTER_BLOCKS;
 
 	foreach ($registerBlocks as $registerBlock) {
-		//find the block template file in the correct theme directory
-		//locate_template only works with files, not folders, so to ensure we get the folder from correct location we are filtering the path.
+		// Find the block template file in the correct theme directory
+		// locate_template only works with files, not folders, so to ensure we get the folder from correct location we are filtering the path.
 		$path = locate_template('components/component_' .$registerBlock.'/'.$registerBlock.'.php');
 		$path = str_replace('/'.$registerBlock.'.php', '', $path);
 		
