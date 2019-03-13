620267725a3823dca1137356e7966fc78f525f09
oka@mauderewrite.com <oka@mauderewrite.com>
2019-03-13 17:28:39 -0400

    placeholder twitter and instagram components

---
+++ components/component_twitter/twitter.php
@@ -0,0 +1,45 @@
+<?php // Global Twitter ?>
+
+<div class="component-twitter component margin-small-bottom-1x">
+	<h4 class="headline6">Live Update</h4>
+	<ul class="twitter-items">
+		<li class="twitter-item padding-small-top-2x margin-small-top-2x component-theme-top-border">
+			<div class="twitter-header margin-small-bottom-1x">
+				<a href="https://twitter.com/AcreageCannabis"><img src="https://pbs.twimg.com/profile_images/997535238929596417/oGzKf9Wt_bigger.jpg" /></a>
+				<p class="subheadline2"><a href="https://twitter.com/AcreageCannabis">Acreage Holdings</a></p>
+			</div>
+			<div class="twitter-post">
+				<p class="color-text-black">Acreage Holdings Enters California Dispensary Market With Acquisition of Kanna, Inc.</p>
+			</div>
+		</li>
+		<li class="twitter-item padding-small-top-2x margin-small-top-2x component-theme-top-border">
+			<div class="twitter-header margin-small-bottom-1x">
+				<a href="https://twitter.com/AcreageCannabis"><img src="https://pbs.twimg.com/profile_images/997535238929596417/oGzKf9Wt_bigger.jpg" /></a>
+				<p class="subheadline2"><a href="https://twitter.com/AcreageCannabis">Acreage Holdings</a></p>
+			</div>
+			<div class="twitter-post">
+				<p class="color-text-black">Acreage Holdings Enters California Dispensary Market With Acquisition of Kanna, Inc.</p>
+			</div>
+		</li>
+		<li class="twitter-item padding-small-top-2x margin-small-top-2x component-theme-top-border">
+			<div class="twitter-header margin-small-bottom-1x">
+				<a href="https://twitter.com/AcreageCannabis"><img src="https://pbs.twimg.com/profile_images/997535238929596417/oGzKf9Wt_bigger.jpg" /></a>
+				<p class="subheadline2"><a href="https://twitter.com/AcreageCannabis">Acreage Holdings</a></p>
+			</div>
+			<div class="twitter-post">
+				<p class="color-text-black">Acreage Holdings Enters California Dispensary Market With Acquisition of Kanna, Inc.</p>
+			</div>
+		</li>
+		<li class="twitter-item padding-small-top-2x margin-small-top-2x component-theme-top-border">
+			<div class="twitter-header margin-small-bottom-1x">
+				<a href="https://twitter.com/AcreageCannabis"><img src="https://pbs.twimg.com/profile_images/997535238929596417/oGzKf9Wt_bigger.jpg" /></a>
+				<p class="subheadline2"><a href="https://twitter.com/AcreageCannabis">Acreage Holdings</a></p>
+			</div>
+			<div class="twitter-post">
+				<p class="color-text-black">Acreage Holdings Enters California Dispensary Market With Acquisition of Kanna, Inc.</p>
+			</div>
+		</li>
+	</ul>
+	<!-- acreagetodo: twitter feed -->
+</div>
+
