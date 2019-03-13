56f77c6fc0c127cffdfa3efc206b1727d1e602ca
oka@mauderewrite.com <oka@mauderewrite.com>
2019-03-13 18:13:31 -0400

    wip: share component

---
+++ components/component_share/share.php
@@ -0,0 +1,13 @@
+<?php 
+	// Share
+?>
+
+<div class="component-share component">
+	<?php if($this->displayTitle){ ?>
+		<h4 class="caption2 color-text-primary uppercase display-block"><?= $this->displayTitle;?></h4>
+	<?php } ?>
+	<div class="share-tools">
+		
+	</div>
+</div>
+
