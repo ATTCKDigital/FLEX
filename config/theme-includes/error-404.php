84ebb35448263d20f8fc489f8bd606a597e26eb7
oka@mauderewrite.com <oka@mauderewrite.com>
2019-03-13 17:28:24 -0400

    add configurable 404 page message

--- config/theme-includes/error-404.php
+++ config/theme-includes/error-404.php
@@ -1,2 +1,5 @@
 <?php //404 Error Message ?>
-<h1 class="headline2">We're sorry, the content you are looking for cannot be found. Please check the url and try again.</h1>
+<h1 class="headline3 margin-small-bottom-2x"><?= get_field('404_page_title', 'options');?></h1>
+<p class="headline6">
+	<?= get_field('404_page_message', 'options');?>
+</p>
