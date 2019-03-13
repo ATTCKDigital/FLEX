b1095719ab7e80d034b255d793ad0add06678d6d
oka@mauderewrite.com <oka@mauderewrite.com>
2019-03-13 11:34:45 -0400

    archive feed header

---
+++ components/component_archive-feed/archive-feed-header.php
@@ -0,0 +1,11 @@
+<section class="component-row padding-small-top-7x component" data-component-name="LoadMore">
+	<div class="pure-g component-row-wide component-alignment-top">
+		<div class="column flex-g-lg-12-12 flex-g-md-12-12 flex-g-sm-12-12">
+			<div class="component-archive-feed">
+				<header class="archive-feed-header">
+                    <h1 class="headline2 color-text-primary uppercase align-center"><?= $this->title;?></h1>
+                </header>
+            </div>
+        </div>
+    </div>
+</section>
