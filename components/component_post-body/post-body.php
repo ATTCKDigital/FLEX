f16a1dc6650308f359218fd396a3e1788e14c903
oka@mauderewrite.com <oka@mauderewrite.com>
2019-03-14 15:23:37 -0400

    make share url and title more flexible

--- components/component_post-body/post-body.php
+++ components/component_post-body/post-body.php
@@ -13,17 +13,17 @@
 			</div>
 		</div>
 	</div>
-	<div class="pure-g component-row-wide component-alignment-top">
-		<div class="column flex-g-lg-12-12 flex-g-md-12-12 flex-g-sm-12-12">
-			<div class="component-post-body">
-				<?php if(has_post_thumbnail()){ ?>
+	<?php if(has_post_thumbnail()){ ?>
+		<div class="pure-g component-row-wide component-alignment-top">
+			<div class="column flex-g-lg-12-12 flex-g-md-12-12 flex-g-sm-12-12">
+				<div class="component-post-body">
 					<div class="featured-image margin-small-bottom-3x">
 						<?php the_post_thumbnail('full');?>
 					</div>
-				<?php } ?>
+				</div>
 			</div>
-		</div>
-	</div>	
+		</div>	
+	<?php } ?>
 	<div class="pure-g component-row-wide component-alignment-top">
 		<div class="column flex-g-lg-2-12 flex-g-md-1-12 flex-g-sm-0-12"></div>
 		<div class="column flex-g-lg-8-12 flex-g-md-10-12 flex-g-sm-12-12">
@@ -35,8 +35,17 @@
 					<span class="caption2 color-text-primary uppercase display-block"><?php the_tags('', ', ', ''); ?></span>
 				</div>
 				<?php
+					$postTitle = get_field('og_title');
+
+					if($postTitle) {
+						$postTitle = $postTitle;
+					} else {
+						$postTitle = get_the_title();
+					}
 					echo Utils::render_template('components/component_share/share.php', array(
-						"displayTitle" => 'Share'
+						"displayTitle" 	=> 'Share',
+						"socialTitle" 	=> $postTitle,
+						"url" 			=> get_the_permalink()
 					));
 				?>
 			</div>
--- components/component_share/share.php
+++ components/component_share/share.php
@@ -1,17 +1,21 @@
 <?php 
 	// Share
 
-	$url = get_permalink();
+	$url = $this->url;
 	$encoded = urlencode($url);
 ?>
 
 <div class="component-share component" data-component-name="Share">
 	<?php if($this->displayTitle){ ?>
-		<h4 class="caption2 color-text-primary uppercase display-block margin-small-bottom-1x"><?= $this->displayTitle;?></h4>
+		<h4 class="caption2 color-text-primary uppercase display-block margin-small-right-1x"><?= $this->displayTitle;?></h4>
 	<?php } ?>
-	<div class="share-tools">
-		<a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $encoded;?>" target="_blank" data-options="menubar=1,resizable=1,width=600,height=400" class="margin-right-10 share-button share-facebook sharelink"><i class="fab fa-facebook-f fa-2x"></i></a>
-		<a href="https://twitter.com/home/?status=<?php the_title() ?>: <?php the_permalink() ?> via @<?= get_field('twitter_username', 'options');?>" target="_blank" data-options="menubar=1,resizable=1,width=600,height=400" class="share-button share-twitter sharelink"><i class="fab fa-twitter fa-2x"></i></a>
+	<div class="share-tools-wrapper">
+		<span class="share-expand display-block shareExpand"><i class="fas fa-share"></i></span>
+		<div class="share-tools">
+			<a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $encoded;?>" target="_blank" data-options="menubar=1,resizable=1,width=600,height=400" class="margin-right-10 share-button share-facebook sharelink"><i class="fab fa-facebook-f fa-2x"></i></a>
+			<a href="https://twitter.com/home/?status=<?= $this->socialTitle ?>: <?= $encoded; ?> via @<?= get_field('twitter_username', 'options');?>" target="_blank" data-options="menubar=1,resizable=1,width=600,height=400" class="share-button share-twitter sharelink"><i class="fab fa-twitter fa-2x"></i></a>
+			<a href="mailto:?subject=I thought you might be interested in <?= $this->socialTitle ?>&body=<?= $this->socialTitle ?> <?= $encoded; ?>" data-options="menubar=1,resizable=1,width=600,height=400" class="share-button share-email sharelink"><i class="far fa-envelope"></i></a>
+		</div>
 	</div>
 </div>
 
