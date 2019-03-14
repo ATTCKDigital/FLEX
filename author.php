0b3cee5a4166145358dcc7b4192f4760372c99a6
oka@mauderewrite.com <oka@mauderewrite.com>
2019-03-14 15:24:28 -0400

    author/user page

--- author.php
+++ author.php
@@ -1,21 +1,14 @@
 <?php
 /**
  * Template: Author Archive
- * Description: Wordpress template for author archive page.
+ * Description: Wordpress template for an author page. 
  *
- */
+**/
     get_header();
 
-
-    $maxPages = $wp_query->max_num_pages; //Find the max number of pages for the query, necessary for "Load More"
-    $authorID = get_the_author_meta('ID'); //Find the author's ID
-
-    echo Utils::render_template("components/component_author-feed.php", array(
-        "maxPages"      => $maxPages,
-        "authorID"      => $authorID,
-        "query"         => $wp_query, // $wp_query is the default WordPress query. It uses the posts per page set in wp-admin.
-        "postType"      => 'post', //Set post type to be displayed for the author
-    ));
-
+    $postID = get_the_ID();
+            
+        echo Utils::render_template('components/component_author/author.php');
+    
     get_footer();
 ?>
