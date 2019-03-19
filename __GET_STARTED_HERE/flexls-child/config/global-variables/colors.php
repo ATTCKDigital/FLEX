5745fc2a308138c3f6e77120211c82016127784b
oka@mauderewrite.com <oka@mauderewrite.com>
2019-03-19 17:51:02 -0400

    update default colors

--- __GET_STARTED_HERE/flexls-child/config/global-variables/colors.php
+++ __GET_STARTED_HERE/flexls-child/config/global-variables/colors.php
@@ -8,110 +8,60 @@
 $colors = array(
 	array(
 		'default'     => '#2F2F2F',
-		'description' => 'Set your site\'s primary color here.',
-		'label'       => 'Primary Color',
-		'slug'        => 'primary-color-1',
-		'theme'				=> 'boilerplate',
-	),
-	array(
-		'default'     => '#7D7D7D',
-		'description' => 'Set your site\'s primary hover color here.',
-		'label'       => 'Primary Color Hover',
-		'slug'        => 'primary-color-1-hover',
-		'theme'				=> 'boilerplate',
+		'description' => 'Set your site\'s primary text color here.',
+		'label'       => 'Primary Text Color',
+		'slug'        => 'primary-text-color',
 	),
 	array(
 		'default'     => '#FFFFFF',
-		'description' => 'Set your site\'s first secondary color here.',
-		'label'       => 'Secondary Color 1',
-		'slug'        => 'secondary-color-1',
-		'theme'				=> 'boilerplate',
+		'description' => 'Set your site\'s secondary text color here.',
+		'label'       => 'Secondary Text Color',
+		'slug'        => 'secondary-text-color',
 	),
-	array(
-		'default'     => '#E7E7E7',
-		'description' => 'Set your site\'s first secondary hover color here.',
-		'label'       => 'Secondary Color 1 Hover',
-		'slug'        => 'secondary-color-1-hover',
-		'theme'				=> 'boilerplate',
-	),
+
 	array(
 		'default'     => '#F5F5F5',
-		'description' => 'Set your site\'s second secondary color here.',
-		'label'       => 'Secondary Color 2',
-		'slug'        => 'secondary-color-2',
-		'theme'				=> 'boilerplate',
+		'description' => 'Set your site\'s tertiary text color here.',
+		'label'       => 'Tertiary Text Color',
+		'slug'        => 'tertiary-text-color',
 	),
-	array(
-		'default'     => '#D9D9D9',
-		'description' => 'Set your site\'s second secondary hover color here.',
-		'label'       => 'Secondary Color 2 Hover',
-		'slug'        => 'secondary-color-2-hover',
-		'theme'				=> 'boilerplate',
-	),
-	array(
-		'default'     => '#2F2F2F',
-		'description' => 'Set your site\'s first background color here.',
-		'label'       => 'Background Color 1',
-		'slug'        => 'background-color-1',
-		'theme'				=> 'boilerplate',
-	),
-	array(
-		'default'     => '#FFFFFF',
-		'description' => 'Set your site\'s second background color here.',
-		'label'       => 'Background Color 2',
-		'slug'        => 'background-color-2',
-		'theme'				=> 'boilerplate',
-	),
+
 	array(
 		'default'     => '#F5F5F5',
-		'description' => 'Set your site\'s third background color here.',
-		'label'       => 'Background Color 3',
-		'slug'        => 'background-color-3',
-		'theme'				=> 'boilerplate',
-	),
-	array(
-		'default'     => '#2F2F2F',
-		'description' => 'Set your site\'s first text color here.',
-		'label'       => 'Text Color 1',
-		'slug'        => 'text-color-1',
-		'theme'				=> 'boilerplate',
+		'description' => 'Set your site\'s primary background color here.',
+		'label'       => 'Primary Background Color',
+		'slug'        => 'primary-background-color',
 	),
 	array(
 		'default'     => '#7D7D7D',
-		'description' => 'Set your site\'s first text hover color here.',
-		'label'       => 'Text Color 1 Hover',
-		'slug'        => 'Text-color-1-hover',
-		'theme'				=> 'boilerplate',
+		'description' => 'Set your site\'s secondary background color here.',
+		'label'       => 'Secondary Background Color',
+		'slug'        => 'secondary-background-color',
 	),
+
+	array(
+		'default'     => '#F6F6F6',
+		'description' => 'Set your site\'s tertiary background color here.',
+		'label'       => 'Tertiary Background Color',
+		'slug'        => 'tertiary-background-color',
+	),
+
 	array(
 		'default'     => '#FFFFFF',
-		'description' => 'Set your site\'s second text color here.',
-		'label'       => 'Text Color 2',
-		'slug'        => 'text-color-2',
-		'theme'				=> 'boilerplate',
+		'description' => 'Default White',
+		'label'       => 'Default White Text Color',
+		'slug'        => 'white-text-color',
 	),
+
 	array(
-		'default'     => '#CDCDCD',
-		'description' => 'Set your site\'s second text hover color here.',
-		'label'       => 'Text Color 2 Hover',
-		'slug'        => 'Text-color-2-hover',
-		'theme'				=> 'boilerplate',
+		'default'     => '#000000',
+		'description' => 'Default Black',
+		'label'       => 'Default Black Text Color',
+		'slug'        => 'black-text-color',
 	),
-	array(
-		'default'     => '#7D7D7D',
-		'description' => 'Set your site\'s third text color here.',
-		'label'       => 'Text Color 3',
-		'slug'        => 'text-color-3',
-		'theme'				=> 'boilerplate',
-	),
-	array(
-		'default'     => '#C1C1C1',
-		'description' => 'Set your site\'s third text hover color here.',
-		'label'       => 'Text Color 3 Hover',
-		'slug'        => 'Text-color-3-hover',
-		'theme'				=> 'boilerplate',
-	),
+
 );
+
 
 if(!defined('FLEXLS_COLORS')) {
 	define('FLEXLS_COLORS', $colors);
