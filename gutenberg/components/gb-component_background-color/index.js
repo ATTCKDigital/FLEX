3860526595675ec706ab2eb6b8d89223bc2d4191
oka@mauderewrite.com <oka@mauderewrite.com>
2019-03-21 18:13:43 -0400

    clean up

--- gutenberg/components/gb-component_background-color/index.js
+++ gutenberg/components/gb-component_background-color/index.js
@@ -33,38 +33,21 @@ export {
 function BackgroundColorOptions( props ) {
 	
 	const setBackgroundColor = value => props.setAttributes( { backgroundColor: value } );
-
-
-
-	const colorPanelSelect = () => {
 		
 		return (
+			
 			<PanelColorSettings
 				title={ __( 'Background Color' ) }
 				colorSettings={ [
-						{
-							value: props.attributes.backgroundColor,
-							onChange: setBackgroundColor,
-							label: __( 'Background Color' ),
-						}
+					{
+						value: props.attributes.backgroundColor,
+						onChange: setBackgroundColor,
+						label: __( 'Background Color' ),
+					}
 				] }
 			>
 			</PanelColorSettings>
 		);
-	};
-
-	return (
-		<PanelBody
-			title={ __( 'Background Color' ) }
-			className="wds-background-color-options"
-			initialOpen={ false }
-		>
-
-			<PanelRow>
-				{ colorPanelSelect() }
-			</PanelRow>
-		</PanelBody>
-	);
 }
 
 export default BackgroundColorOptions;
