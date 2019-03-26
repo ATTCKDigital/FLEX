c0fb3e53774a78d84aedfd6adcc1d613397e159c
oka@mauderewrite.com <oka@mauderewrite.com>
2019-03-26 17:55:16 -0400

    wip: add feed block

---
+++ gutenberg/blocks/block_feed/feed.js
@@ -0,0 +1,156 @@
+/**
+ * Block dependencies
+ */
+import classnames from 'classnames';
+import icons from 'icons';
+
+/**
+ * Internal block libraries
+ */
+const { __ } = wp.i18n;
+const {
+	registerBlockType,
+} = wp.blocks;
+const {
+	RichText,
+	AlignmentToolbar,
+	BlockControls,
+	BlockAlignmentToolbar,
+	InspectorControls,
+	InnerBlocks,
+} = wp.editor;
+const {
+	Toolbar,
+	Button,
+	ButtonGroup,
+	Tooltip,
+	PanelBody,
+	PanelRow,
+	TextControl,
+	Spinner,
+	SelectControl,
+
+} = wp.components;
+const { 
+	withSelect 
+} = wp.data;
+
+
+/**
+ * Internal dependencies
+ */
+// Import all of our Margin Options requirements.
+import MarginOptions, { MarginOptionsAttributes, MarginOptionsClasses } from '../../components/gb-component_margin';
+// Import all of our Text Color Options requirements.
+import PaddingOptions, { PaddingOptionsAttributes, PaddingOptionsClasses } from '../../components/gb-component_padding';
+
+/**
+	* Register block
+ */
+export default registerBlockType(
+	'flexls/feed',
+	{
+		title: __( 'Feed' ),
+		description: __( 'A feed of posts.' ),
+		category: 'common',
+		icon: icons.feed,
+		parent: 'flexls/column',
+		keywords: [
+			__( 'Feed', 'flexls' ),
+			__( 'Archive', 'flexls' ),
+			__( 'Posts', 'flexls' ),
+		],
+		attributes: {
+			...MarginOptionsAttributes,
+			...PaddingOptionsAttributes,
+		},
+		edit: withSelect( select => {
+				const {getPostTypes} = select('core');
+
+                return {
+
+			        typesList: getPostTypes(),
+                    posts: select( 'core' ).getEntityRecords( 'postType', 'post', { per_page: 3 } )
+                };
+            } )( ( { posts, className, isSelected, setAttributes, typesList } ) => {
+                if ( ! posts ) {
+                    return (
+                        <p className={className} >
+                            <Spinner />
+                            { __( 'Loading Posts', 'flexls' ) }
+                        </p>
+                    );
+                }
+                if ( 0 === posts.length ) {
+                    return <p>{ __( 'No Posts', 'flexls' ) }</p>;
+                }
+                return (
+	                <InspectorControls>
+		                <PanelBody title={ __( 'Feed Post Type' ) }>
+			                <PanelRow>
+								<SelectControl
+									key="post-type"
+									label={ __( 'Post Type' ) }
+									options={ [
+										{
+											label: __( 'Post' ),
+											value: 'post',
+										},
+										
+									] }
+								/>
+							</PanelRow>
+			                <PanelRow>
+								<SelectControl
+									key="feed-categroy"
+									label={ __( 'Feed Category' ) }
+									options={ [
+										{
+											label: __( 'Post' ),
+											value: 'post',
+										},
+										
+									] }
+								/>
+							</PanelRow>
+							<PanelRow>
+								<SelectControl
+									key="feed-number"
+									label={ __( 'Number of posts to display' ) }
+									options={ [
+										{
+											label: __( 'Post' ),
+											value: 'post',
+										},
+										
+									] }
+								/>
+							</PanelRow>
+						</PanelBody>
+					</InspectorControls>,
+                	//okatodo: Make an Inspector control panel with post type and category selection and push that info back into the posts above
+	                <div className={ 'component-archive-feed' }>
+		                <div className={ 'feed-items' }>
+	                        { posts.map( post => {
+	                            return (
+	                                 <div className={ 'feed-item' }>
+	                                    <h2 class="headline6">
+	                                    	<a className={ className } href={ post.link }>
+		                                        { post.title.rendered }
+		                                    </a>
+		                                </h2>
+	                                </div>
+	                            );
+	                        }) }
+		                </div>
+		            </div>
+                );
+            } ) // end withAPIData
+        , // end edit
+
+		save() {
+			return null;
+		},
+
+	},
+);
