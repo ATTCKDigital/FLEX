583deac36f9a0e0ee5ad171668e752edb0e609f1
Sarah Quigley <sarah@sarahquigley.net>
2019-03-28 14:28:50 -0400

    fix: fix column medium output issue

--- gutenberg/components/gb-component_columns/classes.php
+++ gutenberg/components/gb-component_columns/classes.php
@@ -4,7 +4,7 @@ namespace FLEX_LAYOUT_SYSTEM\Components\Columns;
 function column_options_classes( $attributes ) {
 	$class = '';
 	$class .= array_key_exists('columnSmall', $attributes) ? " flex-g-sm-{$attributes['columnSmall']}-{$attributes['columnCount']}" : '';
-	$class .= array_key_exists('columnMedium,', $attributes) ? " flex-g-md-{$attributes['columnMedium']}-{$attributes['columnCount']}" : '';
+	$class .= array_key_exists('columnMedium', $attributes) ? " flex-g-md-{$attributes['columnMedium']}-{$attributes['columnCount']}" : '';
 	$class .= array_key_exists('columnLarge', $attributes) ? " flex-g-lg-{$attributes['columnLarge']}-{$attributes['columnCount']}" : '';
 	$class .= array_key_exists('columnXL', $attributes) ? " flex-g-xl-{$attributes['columnXL']}-{$attributes['columnCount']}" : '';
 	return $class;
