c57831ec5a6954911045b393d15da34fd2f1e04e
oka@mauderewrite.com <oka@mauderewrite.com>
2019-04-05 14:15:06 -0400

    add social media style option, fix nav height bug

--- components/component_social-media/social-media.php
+++ components/component_social-media/social-media.php
@@ -17,28 +17,28 @@
 	<?php } ?>
 	<div class="social-media-list <?= $this->colorClass;?> <?= $this->alignment;?>">
 		<?php if($twitter) { ?>
-			<mark class="social-icon margin-global-right-2x"><a href="<?= $twitter;?>" target="_blank"><i class="fab fa-twitter"></i></a></mark>
+			<mark class="social-icon margin-global-right-2x <?= $this->iconStyle;?>"><a href="<?= $twitter;?>" target="_blank"><i class="fab fa-twitter"></i></a></mark>
 		<?php } ?>
 		<?php if($instagram) { ?>
-			<mark class="social-icon margin-global-right-2x"><a href="<?= $instagram;?>" target="_blank"><i class="fab fa-instagram"></i></a></mark>
+			<mark class="social-icon margin-global-right-2x <?= $this->iconStyle;?>"><a href="<?= $instagram;?>" target="_blank"><i class="fab fa-instagram"></i></a></mark>
 		<?php } ?>
 		<?php if($facebook) { ?>
-			<mark class="social-icon margin-global-right-2x"><a href="<?= $facebook;?>" target="_blank"><i class="fab fa-facebook-f"></i></a></mark>
+			<mark class="social-icon margin-global-right-2x <?= $this->iconStyle;?>"><a href="<?= $facebook;?>" target="_blank"><i class="fab fa-facebook-f"></i></a></mark>
 		<?php } ?>
 		<?php if($linkedin) { ?>
-			<mark class="social-icon margin-global-right-2x"><a href="<?= $linkedin;?>" target="_blank"><i class="fab fa-linkedin-in"></i></a></mark>
+			<mark class="social-icon margin-global-right-2x <?= $this->iconStyle;?>"><a href="<?= $linkedin;?>" target="_blank"><i class="fab fa-linkedin-in"></i></a></mark>
 		<?php } ?>
 		<?php if($medium) { ?>
-			<mark class="social-icon margin-global-right-2x"><a href="<?= $medium;?>" target="_blank"><i class="fab fa-medium-m"></i></a></mark>
+			<mark class="social-icon margin-global-right-2x <?= $this->iconStyle;?>"><a href="<?= $medium;?>" target="_blank"><i class="fab fa-medium-m"></i></a></mark>
 		<?php } ?>
 		<?php if($youtube) { ?>
-			<mark class="social-icon margin-global-right-2x"><a href="<?= $youtube;?>" target="_blank"><i class="fab fa-youtube"></i></a></mark>
+			<mark class="social-icon margin-global-right-2x <?= $this->iconStyle;?>"><a href="<?= $youtube;?>" target="_blank"><i class="fab fa-youtube"></i></a></mark>
 		<?php } ?>
 		<?php if($pinterest) { ?>
-			<mark class="social-icon margin-global-right-2x"><a href="<?= $pinterest;?>" target="_blank"><i class="fab fa-pinterest-p"></i></a></mark>
+			<mark class="social-icon margin-global-right-2x <?= $this->iconStyle;?>"><a href="<?= $pinterest;?>" target="_blank"><i class="fab fa-pinterest-p"></i></a></mark>
 		<?php } ?>
 		<?php if($github) { ?>
-			<mark class="social-icon margin-global-right-2x"><a href="<?= $github;?>" target="_blank"><i class="fab fa-github"></i></a></mark>
+			<mark class="social-icon margin-global-right-2x <?= $this->iconStyle;?>"><a href="<?= $github;?>" target="_blank"><i class="fab fa-github"></i></a></mark>
 		<?php } ?>
 	</div>
 </div>
