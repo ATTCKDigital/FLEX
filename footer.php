7dd5e1cb5824544aa675900f6372377d44a42b7d
oka@mauderewrite.com <oka@mauderewrite.com>
2019-04-11 12:03:41 -0400

    fix spacing in footer

--- footer.php
+++ footer.php
@@ -1,9 +1,8 @@
-  </main><!-- Close site <main> -->
+	</main><!-- Close site <main> -->
 </div> <!-- Close content-container div -->
 <footer class="global-footer">
 	<?php echo Utils::render_template('components/component_footer/footer.php'); ?>
 </footer>
-
 <?php wp_footer(); ?>
 </body>
 </html>
