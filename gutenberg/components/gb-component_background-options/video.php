3862be9ac299043a640908d3654991c3fee516d8
oka@mauderewrite.com <oka@mauderewrite.com>
2019-04-11 19:13:51 -0400

    undoing an accidental git mishap

--- gutenberg/components/gb-component_background-options/video.php
+++ gutenberg/components/gb-component_background-options/video.php
@@ -5,7 +5,7 @@ namespace FLEX_LAYOUT_SYSTEM\Components\BackgroundOptions;
 function background_options_video_output( $attributes ) {
 	$video = '';
 	if ( array_key_exists('backgroundType', $attributes) && $attributes['backgroundType'] == 'video' && array_key_exists('backgroundVideo', $attributes) ) {
-		$video = "<video class=\"video-container video-container-overlay\" autoPlay=\"true\" loop=\"true\" muted=\"true\"><source type=\"video/mp4\" src=\"{$attributes['backgroundVideo']['url']}\" /></video>";
+		$video = "<div class=\"video-overlay\"></div><video class=\"video-container video-container-overlay\" autoPlay=\"true\" loop=\"true\" muted=\"true\"><source type=\"video/mp4\" src=\"{$attributes['backgroundVideo']['url']}\" /></video>";
 	}
 	return $video;
 }
