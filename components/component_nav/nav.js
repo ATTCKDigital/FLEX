29b37bf1916b0f2a48457e101a7204083a3f88ac
oka@mauderewrite.com <oka@mauderewrite.com>
2019-04-29 15:57:23 -0400

    add additional scrolling parameters

--- components/component_nav/nav.js
+++ components/component_nav/nav.js
@@ -49,6 +49,13 @@ function Nav($el) {
 			// Show/hide nav bar background color
 				var scroll = data.currentScrollTop;
 
+				//add a class after short scroll to add background color etc
+				if (scroll >= 10) {
+					if (!$('body').hasClass('backgroundNav')) {
+						$('body').addClass('backgroundNav');
+					}
+				}
+
 				// Hide nav entirely once scrolled past a certain distance
 				if (scroll >= 300) {
 					if (!$('body').hasClass('hideNav')) {
@@ -59,6 +66,11 @@ function Nav($el) {
 				// Show again as soon as they start scrolling back up
 				if (data.scrollDirection === 'up') {
 					$('body').removeClass('hideNav');
+				}
+
+				// Show again as soon as they start scrolling back up
+				if (data.scrollDirection === 'up' && scroll <= 10) {
+					$('body').removeClass('hideNav backgroundNav');
 				}
 		});
 	}
