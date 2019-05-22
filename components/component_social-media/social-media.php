cef85095ab7b24f4690005f9f032fc4613c6f58f
oka@mauderewrite.com <oka@mauderewrite.com>
2019-05-22 13:48:51 -0400

    only show component if social media exists

--- components/component_social-media/social-media.php
+++ components/component_social-media/social-media.php
@@ -13,34 +13,36 @@
 ?>
 
 <div class="component-social-media component">
-	<?php if($this->displayTitle){ ?>
-		<h4 class="<?= $this->displayTitleClass;?> <?= $this->alignment;?> margin-global-bottom-2x"><?= $this->displayTitle;?></h4>
+	<?php if($facebook || $twitter || $instagram || $linkedin || $medium || $youtube || $pinterest || $github) { ?>
+		<?php if($this->displayTitle){ ?>
+			<h4 class="<?= $this->displayTitleClass;?> <?= $this->alignment;?> margin-global-bottom-2x"><?= $this->displayTitle;?></h4>
+		<?php } ?>
+		<div class="social-media-list <?= $this->colorClass;?> <?= $this->alignment;?>">
+			<?php if($twitter) { ?>
+				<mark class="social-icon margin-global-right-2x <?= $this->iconStyle;?>"><a href="<?= $twitter;?>" target="_blank"><i class="fab fa-twitter"></i></a></mark>
+			<?php } ?>
+			<?php if($instagram) { ?>
+				<mark class="social-icon margin-global-right-2x <?= $this->iconStyle;?>"><a href="<?= $instagram;?>" target="_blank"><i class="fab fa-instagram"></i></a></mark>
+			<?php } ?>
+			<?php if($facebook) { ?>
+				<mark class="social-icon margin-global-right-2x <?= $this->iconStyle;?>"><a href="<?= $facebook;?>" target="_blank"><i class="fab fa-facebook-f"></i></a></mark>
+			<?php } ?>
+			<?php if($linkedin) { ?>
+				<mark class="social-icon margin-global-right-2x <?= $this->iconStyle;?>"><a href="<?= $linkedin;?>" target="_blank"><i class="fab fa-linkedin-in"></i></a></mark>
+			<?php } ?>
+			<?php if($medium) { ?>
+				<mark class="social-icon margin-global-right-2x <?= $this->iconStyle;?>"><a href="<?= $medium;?>" target="_blank"><i class="fab fa-medium-m"></i></a></mark>
+			<?php } ?>
+			<?php if($youtube) { ?>
+				<mark class="social-icon margin-global-right-2x <?= $this->iconStyle;?>"><a href="<?= $youtube;?>" target="_blank"><i class="fab fa-youtube"></i></a></mark>
+			<?php } ?>
+			<?php if($pinterest) { ?>
+				<mark class="social-icon margin-global-right-2x <?= $this->iconStyle;?>"><a href="<?= $pinterest;?>" target="_blank"><i class="fab fa-pinterest-p"></i></a></mark>
+			<?php } ?>
+			<?php if($github) { ?>
+				<mark class="social-icon margin-global-right-2x <?= $this->iconStyle;?>"><a href="<?= $github;?>" target="_blank"><i class="fab fa-github"></i></a></mark>
+			<?php } ?>
+		</div>
 	<?php } ?>
-	<div class="social-media-list <?= $this->colorClass;?> <?= $this->alignment;?>">
-		<?php if($twitter) { ?>
-			<mark class="social-icon margin-global-right-2x <?= $this->iconStyle;?>"><a href="<?= $twitter;?>" target="_blank"><i class="fab fa-twitter"></i></a></mark>
-		<?php } ?>
-		<?php if($instagram) { ?>
-			<mark class="social-icon margin-global-right-2x <?= $this->iconStyle;?>"><a href="<?= $instagram;?>" target="_blank"><i class="fab fa-instagram"></i></a></mark>
-		<?php } ?>
-		<?php if($facebook) { ?>
-			<mark class="social-icon margin-global-right-2x <?= $this->iconStyle;?>"><a href="<?= $facebook;?>" target="_blank"><i class="fab fa-facebook-f"></i></a></mark>
-		<?php } ?>
-		<?php if($linkedin) { ?>
-			<mark class="social-icon margin-global-right-2x <?= $this->iconStyle;?>"><a href="<?= $linkedin;?>" target="_blank"><i class="fab fa-linkedin-in"></i></a></mark>
-		<?php } ?>
-		<?php if($medium) { ?>
-			<mark class="social-icon margin-global-right-2x <?= $this->iconStyle;?>"><a href="<?= $medium;?>" target="_blank"><i class="fab fa-medium-m"></i></a></mark>
-		<?php } ?>
-		<?php if($youtube) { ?>
-			<mark class="social-icon margin-global-right-2x <?= $this->iconStyle;?>"><a href="<?= $youtube;?>" target="_blank"><i class="fab fa-youtube"></i></a></mark>
-		<?php } ?>
-		<?php if($pinterest) { ?>
-			<mark class="social-icon margin-global-right-2x <?= $this->iconStyle;?>"><a href="<?= $pinterest;?>" target="_blank"><i class="fab fa-pinterest-p"></i></a></mark>
-		<?php } ?>
-		<?php if($github) { ?>
-			<mark class="social-icon margin-global-right-2x <?= $this->iconStyle;?>"><a href="<?= $github;?>" target="_blank"><i class="fab fa-github"></i></a></mark>
-		<?php } ?>
-	</div>
 </div>
 
