fa3ed170f6a27973ccec508465f28ef5010750af
oka@mauderewrite.com <oka@mauderewrite.com>
2019-05-28 17:00:13 -0400

    fix options config

--- __GET_STARTED_HERE/flexlayout-child/config/acf-configs/acf-options-page.php
+++ __GET_STARTED_HERE/flexlayout-child/config/acf-configs/acf-options-page.php
@@ -1,15 +1,10 @@
 <?php
 // custom settings page, use acf to add necessary fields
 if (function_exists('acf_add_options_page')) {
-	acf_add_options_page(
-		[
-			'page_title' => 'Global Settings',
-			'menu_title' => 'Global Settings',
-			'menu_slug'  => 'theme-global-settings',
-			'capability' => 'edit_posts',
-			'position'   => '59.9',//right above Appearance
-			'redirect'   => false
-		]
-	);
+	// add global settings in settings menu
+	acf_add_options_sub_page(array(
+		'page_title' 	=> 'Global Settings',
+		'menu_title' 	=> 'Global Settings',
+		'parent_slug' 	=> 'options-general.php',
+	));
 }
-
