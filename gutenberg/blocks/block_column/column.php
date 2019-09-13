efeabfaff342440558a9cadba94dcfa245932d8e
oka@mauderewrite.com <oka@mauderewrite.com>
2019-09-13 14:24:21 -0400

    add video background to column

--- gutenberg/blocks/block_column/column.php
+++ gutenberg/blocks/block_column/column.php
@@ -76,6 +76,7 @@ function render_column_block($attributes, $content) {
 		$styleBlock = '';
 	}
 
+	//video will only appear if the column has content
 	$innerContent = background_options_video_output($attributes);
 
 	$output = "<section{$id} class=\"{$class}\" data-section-id=\"section-{$sectionDataId}\" style=\"{$style}\">{$styleBlock}{$content}{$innerContent}</section>";
