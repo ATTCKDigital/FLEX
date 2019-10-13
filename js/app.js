c4882655d0727018d969f42be6fa59d0f972c1a7
Dennis Plucinik <dennis.plucinik@gmail.com>
2019-10-12 20:38:59 -0400

    making jQuery global to the child theme

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
+// https://stackoverflow.com/questions/34338411/how-to-import-jquery-using-es6-syntax
 import $ from 'jquery';
+window.$ = $;
+
 import cssVars from 'css-vars-ponyfill';
 import GlobalEvents from './global-events';
 import Loader from './load-components';
@@ -12,10 +15,9 @@ $(function() {
 	GlobalEvents.initGlobalEvents();
 });
 
-//Resolves persisted cache issue in safari
+// Resolves persisted cache issue in safari
 window.onpageshow = function(event) {
 	if (event.persisted) {
-			window.location.reload()
-	} else {
+		window.location.reload()
 	}
 };
