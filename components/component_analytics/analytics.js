8cc5a28b8beb38090a2d90884c821f11ca4b36f7
Dennis Plucinik <dennis.plucinik@gmail.com>
2019-12-07 07:02:04 -0500

    fixing analytics data reference

--- components/component_analytics/analytics.js
+++ components/component_analytics/analytics.js
@@ -43,22 +43,22 @@ function Analytics($el) {
 
 				switch (true) {
 					case classes[i].indexOf('event-category') > -1 :
-						event['category'] = classes[i];
+						event['category'] = classes.category.substr('event-category'.length + 1);
 						match = true;
 						break;
 
 					case classes[i].indexOf('event-action') > -1 :
-						event['action'] = classes[i];
+						event['action'] = classes.action.substr('event-action'.length + 1);
 						match = true;
 						break;
 
 					case classes[i].indexOf('event-label') > -1 :
-						event['label'] = classes[i];
+						event['label'] = classes.label.substr('event-label'.length + 1);
 						match = true;
 						break;
 
 					case classes[i].indexOf('event-value') > -1 :
-						event['value'] = (1 * classes[i]);
+						event['value'] = classes.value.substr('event-value'.length + 1);
 						match = true;
 						break;
 
