13fd8f67bfeac79f85988a78ff634be6b368a5ae
Dennis Plucinik <dennis.plucinik@gmail.com>
2019-12-07 05:56:08 -0500

    adding analytics module

--- header.php
+++ header.php
@@ -52,5 +52,5 @@
 
 	<?php echo Utils::render_template('config/theme-includes/svg-sprite.php'); ?>
 	<?php echo Utils::render_template('components/component_nav/nav.php'); ?>
-	<div class="content-container component" data-component-name="ElementsInViewport">
+	<div class="content-container component" data-component-name="ElementsInViewport Analytics">
 		<main class="content">
