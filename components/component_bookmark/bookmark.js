e8ccc8133797bbd8fff7f4bda1dcc061df0f1006
Dennis Plucinik <dennis.plucinik@gmail.com>
2021-01-04 05:36:51 -0500

    adding bookmark component

---
+++ components/component_bookmark/bookmark.js
@@ -0,0 +1,42 @@
+import $ from 'jquery';
+
+/**
+ * Display a calendar selector
+ */
+function Bookmark($el) {
+	// Cache the body
+	var $body = $('body');
+
+	function bindEvents() {
+		$el = $el;
+
+		$('a[href="/#bookmark"]').on('click', openBookmark);
+	}
+
+	function openBookmark() {
+		if (window.sidebar) { // Mozilla Firefox Bookmark
+			window.sidebar.addPanel(location.href,document.title,"");
+		} else if(window.external) { // IE Favorite
+			window.external.AddFavorite(location.href,document.title); }
+		else if(window.opera && window.print) { // Opera Hotlist
+			this.title=document.title;
+			return true;
+		}
+	}
+
+	this.init = function ($el) {
+		console.log('/FLEX\t/components\t/component_analytics\t/bookmark.js', 'Bookmark.init()');
+
+		// https://stackoverflow.com/questions/22085812/addfavorite-js-doesnt-work-with-chrome
+		// This will only work in IE
+		//bindEvents();
+
+		return this;
+	}
+
+	return this.init($el);
+}
+
+console.log('loaded', '/FLEX\t/components\t/component_analytics\t/bookmark.js');
+
+export default Bookmark;
