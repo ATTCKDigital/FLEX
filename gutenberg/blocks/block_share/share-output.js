6043f8e2e457a763f1d221b7e23212d6f780da78
Dennis Plucinik <dennis.plucinik@gmail.com>
2021-01-04 06:03:23 -0500

    fixing bug where only one share icon was showing

--- gutenberg/blocks/block_share/share-output.js
+++ gutenberg/blocks/block_share/share-output.js
@@ -4,30 +4,38 @@
  * @return {string} The social media output container.
  */
 function ShareOutput( props ) {
+	var facebookLink;
+	var twitterLink;
+	var linkedinLink;
+	var emailLink;
+
 	if ( props.attributes.facebook ) {
-		return (
-			<mark className="social-icon margin-right-2x"><i className="fab fa-facebook-f"></i></mark>
-		);
+		facebookLink = <mark className="social-icon margin-right-2x"><i className="fab fa-facebook-f"></i></mark>;
 	}
 
 	if ( props.attributes.twitter ) {
-		return (
-			<mark className="social-icon margin-right-2x"><i className="fab fa-twitter"></i></mark>
-		);
+		twitterLink = <mark className="social-icon margin-right-2x"><i className="fab fa-twitter"></i></mark>;
 	}
 
 	if ( props.attributes.linkedin ) {
-		return (
-			<mark className="social-icon margin-right-2x"><i className="fab fa-linkedin-in"></i></mark>
-		);
+		linkedinLink = <mark className="social-icon margin-right-2x"><i className="fab fa-linkedin-in"></i></mark>;
 	}
 
 	if ( props.attributes.email ) {
-		return (
-			<mark className="social-icon margin-right-2x"><i className="far fa-email"></i></mark>
-		);
+		emailLink = <mark className="social-icon margin-right-2x"><i className="far fa-email"></i></mark>;
 	}
 
+	return (
+		<div className="component-share component" data-component-name="Share">
+			<div className="share-list">
+				<span className="caption2 display-inline-block uppercase margin-right-2x">Share</span>
+				{facebookLink}
+				{twitterLink}
+				{linkedinLink}
+				{emailLink}
+			</div>
+		</div>
+	)
 }
 
 export default ShareOutput;
