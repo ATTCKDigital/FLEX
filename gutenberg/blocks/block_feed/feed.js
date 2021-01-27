bf51c38af6f5bd262c54ba5e6b5f8f4e3249e937
Ergin Deniz Kosecioglu <deniz@crafton.pl>
2021-01-27 16:06:28 +0100

    feed added back

--- gutenberg/blocks/block_feed/feed.js
+++ gutenberg/blocks/block_feed/feed.js
@@ -20,9 +20,7 @@ const {
 	PanelRow,
 	Spinner,
 	SelectControl,
-	RangeControl,
-	ToggleControl,
-	FormTokenField
+
 } = wp.components;
 const {
 	withSelect
@@ -43,194 +41,103 @@ import PaddingOptions, { PaddingOptionsAttributes, PaddingOptionsClasses } from 
 export default registerBlockType(
 	'flexlayout/feed',
 	{
-		title: __('Feed'),
-		description: __('A feed of posts.'),
+		title: __( 'Feed' ),
+		description: __( 'A feed of posts.' ),
 		category: 'common',
 		icon: icons.feed,
 		// parent: ['flexlayout/column'],
 		keywords: [
-			__('Feed', 'flexlayout'),
-			__('Archive', 'flexlayout'),
-			__('Posts', 'flexlayout'),
+			__( 'Feed', 'flexlayout' ),
+			__( 'Archive', 'flexlayout' ),
+			__( 'Posts', 'flexlayout' ),
 		],
 		attributes: {
-			postType: {
-				type: 'string',
-				default: 'post'
-			},
-			postPerPage: {
-				type: Number,
-				default: 12
-			},
-			columnNumber: {
-				type: Number,
-				default: 3,
-			},
-			categories: {
-				type: 'array',
-				default: []
-			},
-			paginationActive: {
-				type: Boolean,
-				default: true
-			},
-			filterActive: {
-				type: Boolean,
-				default: true
-			},
-			showExcerpt: {
-				type: Boolean,
-				default: false
-			},
 			...MarginOptionsAttributes,
 			...PaddingOptionsAttributes,
 		},
-		edit: withSelect((select, ownProps) => {
-			const { getPostTypes, getEntityRecords } = select('core');
-			const { categories, postType, postPerPage } = ownProps.attributes;
+		edit: withSelect( select => {
+				const {getPostTypes} = select('core');
 
-			const query = {
-				per_page: postPerPage,
-			}
-			return {
+                return {
 
-				typesList: getPostTypes(),
-				posts: getEntityRecords('postType', postType, query),
-				categories: getEntityRecords('taxonomy', 'category')
-			};
-		})(({ posts, categories, className, isSelected, setAttributes, typesList, attributes }) => {
+			        typesList: getPostTypes(),
+                    posts: select( 'core' ).getEntityRecords( 'postType', 'post', { per_page: 3 } )
+                };
+            } )( ( { posts, className, isSelected, setAttributes, typesList } ) => {
+                if ( ! posts ) {
+                    return (
+                        <p className={className} >
+                            <Spinner />
+                            { __( 'Loading Posts', 'flexlayout' ) }
+                        </p>
+                    );
+                }
+                if ( 0 === posts.length ) {
+                    return <p>{ __( 'No Posts', 'flexlayout' ) }</p>;
+                }
+                return (
+	                <InspectorControls>
+		                <PanelBody title={ __( 'Feed Post Type' ) }>
+			                <PanelRow>
+								<SelectControl
+									key="post-type"
+									label={ __( 'Post Type' ) }
+									options={ [
+										{
+											label: __( 'Post' ),
+											value: 'post',
+										},
 
-			if (!posts) {
-				return (
-					<p className={className} >
-						<Spinner />
-						{ __('Loading Posts', 'flexlayout')}
-					</p>
-				);
-			}
-			if (0 === posts.length) {
-				return <p>{__('No Posts', 'flexlayout')}</p>;
-			}
-			return [
-				<InspectorControls>
-				
-					<PanelBody title={__('Post Settings')}>
-						<PanelRow>
-							<SelectControl
-								key="post-type"
-								label={__('Post Type')}
-								value= { attributes.postType ?? 'post' }
-								onChange={ postType => setAttributes( { postType } ) }
-								options= {
-									typesList.map(type => ({
-										label: __(type.name),
-										value: type.slug,
-										
-									}))
-								}
-							/>
-						</PanelRow>
-						<PanelRow>
-							<FormTokenField
-								label="Select Category"
-								value={ attributes.categories.map(category => category.name) }
-								suggestions={ categories && categories.map(category => category.name) }
-								onChange={ tokens => { 
-									const selectedCategories = categories.filter(category => 
-										tokens.some(token => 
-											category.name.toLowerCase() === token.toLowerCase()
-										)
-									)
-												
-									setAttributes({
-										categories: selectedCategories
-									})
-								}}
-								placeholder="Select Categories"
-							/>
-						</PanelRow>
-						<PanelRow>
-							<RangeControl
-									label="Post Per Page"
-									value={ attributes.postPerPage ?? 1 }
-									onChange={ postPerPage => setAttributes( { postPerPage } ) }
-									min={ 1 }
-									max={ 20 }
-							/>
-						</PanelRow>
-						<PanelRow>
-							<RangeControl
-									label="Post Per Row"
-									value={ attributes.columnNumber ?? 3 }
-									onChange={ columnNumber => setAttributes( { columnNumber } ) }
-									min={ 1 }
-									max={ 8 }
-							/>
-						</PanelRow>
-					</PanelBody>
-					<PanelBody title="Options">
-						<PanelRow>
-							<ToggleControl
-								label="Show Pagination"
-								help={ attributes.paginationActive ? 'Pagination Activated' : 'Pagination Deactivated' }
-								checked={ attributes.paginationActive }
-								onChange={ paginationActive => setAttributes( { paginationActive } ) }
-							/>
-						</PanelRow>
-						<PanelRow>
-							<ToggleControl
-								label="Show Filter"
-								help={ attributes.filterActive ? 'Filter Activated' : 'Filter Deactivated' }
-								checked={ attributes.filterActive }
-								onChange={ filterActive => setAttributes( { filterActive } ) }
-							/>
-						</PanelRow>
-						<PanelRow>
-							<ToggleControl
-								label="Show Excerpt"
-								help={ attributes.showExcerpt ? 'Excerpt is active' : 'Filter Deactivated' }
-								checked={ attributes.showExcerpt }
-								onChange={ filterActive => setAttributes( { filterActive } ) }
-							/>
-						</PanelRow>
-					</PanelBody>
-				</InspectorControls>,
-				
-				//okatodo: Make an Inspector control panel with post type and category selection and push that info back into the posts above
-				<div className={classnames(
-						'component-archive-feed',
-					)}>
-					<div>
-						<ul>
-							{attributes.categories && attributes.categories.map(category => {
-								return (
-									<li className={'category-tab'}>
-										{category.name}
-									</li>
-								);
-							})}
-							<li>
-								<a></a>
-							</li>
-						</ul>
-					</div>
-					<ul className={'feed-items'}>
-						{posts.map(post => {
-							return (
-								<li className={'feed-item'} style={{flex: 1 / attributes.columnNumber}}>
-									<div class="is-style-headline6">
-										<a className={className} href={post.link}>
-											{post.title.rendered}
-										</a>
-									</div>
-								</li>
-							);
-						})}
-					</ul>
-				</div>
-			];
-		}) // end withAPIData
-		, // end edit
+									] }
+								/>
+							</PanelRow>
+			                <PanelRow>
+								<SelectControl
+									key="feed-categroy"
+									label={ __( 'Feed Category' ) }
+									options={ [
+										{
+											label: __( 'Post' ),
+											value: 'post',
+										},
+
+									] }
+								/>
+							</PanelRow>
+							<PanelRow>
+								<SelectControl
+									key="feed-number"
+									label={ __( 'Number of posts to display' ) }
+									options={ [
+										{
+											label: __( 'Post' ),
+											value: 'post',
+										},
+
+									] }
+								/>
+							</PanelRow>
+						</PanelBody>
+					</InspectorControls>,
+                	//okatodo: Make an Inspector control panel with post type and category selection and push that info back into the posts above
+	                <div className={ 'component-archive-feed' }>
+		                <div className={ 'feed-items' }>
+	                        { posts.map( post => {
+	                            return (
+	                                 <div className={ 'feed-item' }>
+	                                    <h2 class="headline6">
+	                                    	<a className={ className } href={ post.link }>
+		                                        { post.title.rendered }
+		                                    </a>
+		                                </h2>
+	                                </div>
+	                            );
+	                        }) }
+		                </div>
+		            </div>
+                );
+            } ) // end withAPIData
+        , // end edit
 
 		save() {
 			return null;
--- gutenberg/blocks/block_feed/feed.php
+++ gutenberg/blocks/block_feed/feed.php
@@ -29,34 +29,6 @@ function register_feed_block() {
 					'type' => 'string',
 					'default' => '',
 				],
-				'postType' => [
-					'type' => 'string',
-					'default' => '',
-				],
-				'categories' => [
-					'type' => 'array',
-					'default' => '',
-				],
-				'columnNumber' => [
-					'type' => 'Number',
-					'default' => 4,
-				],
-				'paginationActive' => [
-					'type' => 'bool',
-					'default' => '',
-				],
-				'filterActive' => [
-					'type' => 'Boolean',
-					'default' => true,
-				],
-				'showExcerpt' => [
-					'type' => 'bool',
-					'default' => '',
-				],
-				'postPerPage' => [
-					'type' => 'Number',
-					'default' => 0,
-				],
 			],
 			MARGIN_OPTIONS_ATTRIBUTES,
 			PADDING_OPTIONS_ATTRIBUTES
@@ -72,112 +44,23 @@ function render_feed_block($attributes) {
 	$class = $attributes['className'];
 	$class .= margin_options_classes($attributes);
 	$class .= padding_options_classes($attributes);
-	$categories = $attributes['categories'];
-	$showExcerpt = $attributes['showExcerpt'];
-	$filterActive = $attributes['filterActive'];
-	$columnNumber = $attributes['columnNumber'];
-	$tabs = '';
-	$postFilter = '';
-	$orderby = 'date';
-	$order = $_GET['order'] ?? 'DESC';
-	$pagination = '';
 
-	// Filter
-	if ($filterActive) {
-		$postFilter .= "
-			<div>
-				<label>Sort by:</label>
-				<select name=\"order\" class=\"dropdown\">
-					<option value=\"ASC\" " . ($order == 'ASC' ? 'selected' : '') . ">NEW</option>
-					<option value=\"DESC\" " . ($order == 'DESC' ? 'selected' : '') . ">OLD</option>
-				</select>
-			</div>
-		";	
+	$recent_posts = wp_get_recent_posts( [
+		'numberposts' => 3,
+		'post_status' => 'publish',
+	] );
+
+	if ( empty( $recent_posts ) ) {
+		return '<p>No posts</p>';
 	}
-
-	// Category Tabs
-	if ( !empty( $categories ) ) {
-		$tabs .= "
-		<div class=\"categories\"> 
-			<ul class=\"cat-list\">
-		";
-
-		foreach ( $categories as $category ) {
-			$tabs .= "
-			<li class=\"cat-item\">
-				<button class=\"cat-button\" name=\"category\" value=\"{$category['id']}\">{$category['name']}</button>
-			</li>
-			";
-		}
-
-		$tabs .= "
-			</ul>
-		</div>";
-	}
-
-	// RECENT POSTS
-	$selectedCategory = $_GET['category'] ?? $categories[0]['id'];
-	
-	$paged = get_query_var('paged')
-	? get_query_var('paged')
-	: 1;
-
-	$query = [
-		'posts_per_page' 	=> 	$attributes['postPerPage'],
-		'post_type'				=> 	$attributes['postType'],
-		'post_status' 		=> 	'publish',
-		'cat' 						=> 	$selectedCategory,
-		'order'						=> 	$order,
-		'orderby'					=>	$orderby,
-		'paged' 					=> 	$paged,
-	] ;
-
-	$recent_posts = new \WP_Query( $query );
-
-	if ( !$recent_posts->have_posts() ) {
-		$output = "
-		<div class=\"component-archive-feed {$class}\">
-			{$tabs}
-			{$postFilter}
-			<p>No posts</p>
-		</div>";
-
-		return $output;
-	}
-
-	// Pagination
-
-	$pagination .= "
-		<nav class=\"pagination-nav\" role=\"navigation\" aria-label=\"Pagination Navigation\">" .
-			paginate_links([
-				'base' => str_replace(999999999,
-					'%#%',
-					esc_url(get_pagenum_link(999999999))),
-				'current' => max(1, $paged),
-				'format' => '?paged=%#%',
-				'end_size' => 2,
-				'mid_size' => 4,
-				'prev_next' => true,
-				'prev_text' => '',
-				'next_text' => '',
-				'add_fragment' => '',
-				'total' => $recent_posts->max_num_pages
-			]) .
-		"</nav>";
-
 
 	$feedItems = '';
 
-	while ( $recent_posts->have_posts() ) {
-		$recent_posts -> the_post();
-
-		// $postID  = get_the_ID();
-		$thumbnail = get_post_thumbnail_id();
-		$excerpt = $showExcerpt ? 
-			'<p class="excerpt>' . wp_trim_words( get_the_content(), 60 ) . '</p>' :
-			'';
-
-		$categories = get_the_category();
+	foreach ( $recent_posts as $post ) {
+		$postID  = $post['ID'];
+		$thumbnail = get_post_thumbnail_id($postID);
+		$excerpt = wp_trim_words( get_the_content($postID), 60 );
+		$categories = get_the_category($postID);
 		$arrayCategories =  array();
 
 		if ($categories) {
@@ -188,44 +71,15 @@ function render_feed_block($attributes) {
 		}
 
 		if ($thumbnail) {
-			$thumbnail = '<div class="image-wrapper margin-bottom-1x">'.get_the_post_thumbnail().'</div>';
+			$thumbnail = '<div class="image-wrapper margin-bottom-1x">'.get_the_post_thumbnail($postID).'</div>';
 		} else {
 			$thumbnail = '<div class="image-wrapper margin-bottom-1x no-image"><img src="'.get_field('fallback_image', 'options').'" alt="'.get_field('fallback_image_alt', 'options').'" title="'.get_field('fallback_image_alt', 'options').'" /></div>';
 		}
 		
-		$feedItems  .= '
-			<div class="feed-item" style="width: '. 100 / $columnNumber .'%;">'.
-				$thumbnail.
-				'<div class="feed-info margin-bottom-2x">
-					<span class="eyebrow display-block margin-bottom-1x">'.
-						$displayCategories.
-					'</span>
-					<h2 class="headline6 margin-bottom-1x">'.
-						get_the_title().
-					'</h2>'.
-					$excerpt.
-					'<span class="eyebrow display-block margin-bottom-1x">'.
-						get_the_time('F j, Y').
-					'</span>
-				</div>
-			</div>';
+		$feedItems  .= '<div class="feed-item">'.$thumbnail.'<div class="feed-info margin-bottom-2x"><span class="eyebrow display-block margin-bottom-1x">'.$displayCategories.'</span><h2 class="headline6 margin-bottom-1x">'.get_the_title($postID).'</h2><p class="margin-bottom-1x">'.$excerpt.'</p><span class="eyebrow display-block margin-bottom-1x">'.get_the_time('F j, Y').'</span></div></div>';
 	}
 
-	wp_reset_postdata();
-
-	$permalink = get_the_permalink();
-
-	$output = "
-	<div class=\"component-archive-feed {$class}\" data-component-name=\"FeedFilter\">
-		<form action=\"{$permalink}\" class=\"top-bar filter-form\" method=\"get\">
-			{$tabs}
-			{$postFilter}
-		</form>
-		<div class=\"feed-items load-items padding-bottom-3x\">
-			{$feedItems}
-		</div>
-		{$pagination}
-	</div>";
+	$output = "<div class=\"component-archive-feed {$class}\"><div class=\"feed-items load-items padding-bottom-3x\">{$feedItems}</div>";
 
 	return $output;
 }
