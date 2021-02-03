37d2a8b181bf7ff39bb2f5657c06f82abf898369
Dennis Plucinik <dennis.plucinik@gmail.com>
2021-02-03 02:14:54 -0500

    updating markup to match new semantic naming

--- components/component_post/post-content.php
+++ components/component_post/post-content.php
@@ -1,7 +1,7 @@
 <?php //Single Post ?>
 
 <article role="article" class="component-row padding-top-5x padding-bottom-7x">
-	<div class="flex-grid component-row-wide component-alignment-top">
+	<div class="flex-grid component-row-wide component-row-verticalAlignment-top">
 		<div class="column flex-tablet-landscape-2-12 flex-tablet-portrait-1-12 flex-0-12"></div>
 		<div class="column flex-tablet-landscape-8-12 flex-tablet-portrait-10-12 flex-12-12">
 			<div class="component-post-body">
