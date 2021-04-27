73d364073a5b0246792f07a82116c65f434ed38c
Dennis Plucinik <dennis.plucinik@gmail.com>
2021-04-27 13:42:33 -0400

    adjusting spacing

--- components/component_archive-feed/archive-feed-header.php
+++ components/component_archive-feed/archive-feed-header.php
@@ -1,4 +1,4 @@
-<section class="component-row padding-top-3x padding-tablet-landscape-top-7x component" data-component-name="LoadMore">
+<section class="component-row padding-top-8x padding-tablet-landscape-top-12x component" data-component-name="LoadMore">
 	<div class="flex-grid component-row-wide component-alignment-top">
 		<div class="column flex-tablet-landscape-12-12 flex-tablet-portrait-12-12 flex-12-12">
 			<div class="component-archive-feed">
