3cefd38219d9fb602b006fed654285313439e5ee
Karen Chow <loversandhaters@gmail.com>
2021-09-10 10:44:58 -0400

    repositioning posts block include

--- gutenberg/blocks/blocks.js
+++ gutenberg/blocks/blocks.js
@@ -4,18 +4,22 @@
 import "./block_animated-gif/animated-gif";
 import "./block_button/button";
 import "./block_column/column";
-import "./block_posts/posts";
 import "./block_feed/feed";
 import "./block_heading/heading";
 import "./block_image/image";
 import "./block_list/list";
 import "./block_paragraph/paragraph";
+import "./block_posts/posts";
 import "./block_quote/quote";
 import "./block_row/row";
 import "./block_share/share";
 import "./block_shortcode/shortcode";
 // import "./block_social_media/social-media";
 import "./block_source/source";
+
+// Text block is throwing errors. Disable for now. -DP
+// import "./block_text/text";
+
 // import "./block_users/users";
 import "./block_video/video";
 import "./block_popup/popup";
