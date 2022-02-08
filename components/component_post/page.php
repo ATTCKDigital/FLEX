aab175e1edb8664af619e7c7acdf71dc0a8d7483
Dennis Plucinik II <dennis@attck.com>
2022-02-08 01:46:31 -0500

    increasing page height to accomodate for header overlap

--- components/component_post/page.php
+++ components/component_post/page.php
@@ -1,5 +1,5 @@
 <?php // Plain text page content ?>
-<section class="component-row padding-top-5x padding-bottom-7x">
+<section class="component-row padding-top-15x padding-bottom-7x">
 	<div class="flex-grid component-row-wide component-alignment-top">
 		<div class="column flex-tablet-landscape-2-12 flex-tablet-portrait-1-12 flex-0-12"></div>
 		<div class="column flex-tablet-landscape-8-12 flex-tablet-portrait-10-12 flex-12-12">
