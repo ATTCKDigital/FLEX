3a884807b328c532fe8185d96d6a3f960d5303fb
Dennis Plucinik II <dennis@attck.com>
2025-02-17 14:02:38 -0500

    Helper function to sanitize and validate padding values

--- .githooks/gutenberg/components/gb-component_padding/classes.php
+++ .githooks/gutenberg/components/gb-component_padding/classes.php
@@ -3,27 +3,38 @@
 namespace FLEX_LAYOUT_SYSTEM\Components\Padding;
 
 function padding_options_classes( $attributes ) {
-	$class = '';
-	$class .= array_key_exists('paddingTop', $attributes) && $attributes['paddingTop'] != 'inherit' ? " padding-top-{$attributes['paddingTop']} " : '';
-	$class .= array_key_exists('paddingRight', $attributes) && $attributes['paddingRight'] != 'inherit' ? " padding-right-{$attributes['paddingRight']} " : '';
-	$class .= array_key_exists('paddingLeft', $attributes) && $attributes['paddingLeft'] != 'inherit' ? " padding-left-{$attributes['paddingLeft']} " :'';
-	$class .= array_key_exists('paddingBottom', $attributes) && $attributes['paddingBottom'] != 'inherit' ? " padding-bottom-{$attributes['paddingBottom']} " :'';
-	$class .= array_key_exists('paddingPhoneTop', $attributes) && $attributes['paddingPhoneTop'] != 'inherit' ? " padding-phone-top-{$attributes['paddingPhoneTop']} " :'';
-	$class .= array_key_exists('paddingPhoneRight', $attributes) && $attributes['paddingPhoneRight'] != 'inherit' ? " padding-phone-right-{$attributes['paddingPhoneRight']} " :'';
-	$class .= array_key_exists('paddingPhoneLeft', $attributes) && $attributes['paddingPhoneLeft'] != 'inherit' ? " padding-phone-left-{$attributes['paddingPhoneLeft']} " :'';
-	$class .= array_key_exists('paddingPhoneBottom', $attributes) && $attributes['paddingPhoneBottom'] != 'inherit' ? " padding-phone-bottom-{$attributes['paddingPhoneBottom']} " :'';
-	$class .= array_key_exists('paddingTabletPortraitTop', $attributes) && $attributes['paddingTabletPortraitTop'] != 'inherit' ? " padding-tablet-portrait-top-{$attributes['paddingTabletPortraitTop']} " :'';
-	$class .= array_key_exists('paddingTabletPortraitRight', $attributes) && $attributes['paddingTabletPortraitRight'] != 'inherit' ? " padding-tablet-portrait-right-{$attributes['paddingTabletPortraitRight']} " :'';
-	$class .= array_key_exists('paddingTabletPortraitLeft', $attributes) && $attributes['paddingTabletPortraitLeft'] != 'inherit' ? " padding-tablet-portrait-left-{$attributes['paddingTabletPortraitLeft']} " :'';
-	$class .= array_key_exists('paddingTabletPortraitBottom', $attributes) && $attributes['paddingTabletPortraitBottom'] != 'inherit' ? " padding-tablet-portrait-bottom-{$attributes['paddingTabletPortraitBottom']} " :'';
-	$class .= array_key_exists('paddingTabletLandscapeTop', $attributes) && $attributes['paddingTabletLandscapeTop'] != 'inherit' ? " padding-tablet-landscape-top-{$attributes['paddingTabletLandscapeTop']} " :'';
-	$class .= array_key_exists('paddingTabletLandscapeRight', $attributes) && $attributes['paddingTabletLandscapeRight'] != 'inherit' ? " padding-tablet-landscape-right-{$attributes['paddingTabletLandscapeRight']} " :'';
-	$class .= array_key_exists('paddingTabletLandscapeLeft', $attributes) && $attributes['paddingTabletLandscapeLeft'] != 'inherit' ? " padding-tablet-landscape-left-{$attributes['paddingTabletLandscapeLeft']} " :'';
-	$class .= array_key_exists('paddingTabletLandscapeBottom', $attributes) && $attributes['paddingTabletLandscapeBottom'] != 'inherit' ? " padding-tablet-landscape-bottom-{$attributes['paddingTabletLandscapeBottom']} " :'';
-	$class .= array_key_exists('paddingDesktopTop', $attributes) && $attributes['paddingDesktopTop'] != 'inherit' ? " padding-desktop-top-{$attributes['paddingDesktopTop']} " :'';
-	$class .= array_key_exists('paddingDesktopRight', $attributes) && $attributes['paddingDesktopRight'] != 'inherit' ? " padding-desktop-right-{$attributes['paddingDesktopRight']} " :'';
-	$class .= array_key_exists('paddingDesktopLeft', $attributes) && $attributes['paddingDesktopLeft'] != 'inherit' ? " padding-desktop-left-{$attributes['paddingDesktopLeft']} " :'';
-	$class .= array_key_exists('paddingDesktopBottom', $attributes) && $attributes['paddingDesktopBottom'] != 'inherit' ? " padding-desktop-bottom-{$attributes['paddingDesktopBottom']} " :'';
+    $class = '';
 
-	return $class;
+    // Helper function to sanitize and validate padding values
+    $sanitize_padding = function ($value) {
+        if (!is_string($value)) {
+            return ''; // Handle non-string values
+        }
+        $value = sanitize_text_field($value); // Sanitize to prevent XSS
+        $value = str_replace(' ', '', $value); // Remove spaces
+        if (strlen($value) > 20) {
+            return ''; // Limit length to prevent excessively long class names
+        }
+        return $value;
+    };
+
+    // Process each padding attribute, using the helper function
+    $padding_attributes = [
+        'paddingTop', 'paddingRight', 'paddingLeft', 'paddingBottom',
+        'paddingPhoneTop', 'paddingPhoneRight', 'paddingPhoneLeft', 'paddingPhoneBottom',
+        'paddingTabletPortraitTop', 'paddingTabletPortraitRight', 'paddingTabletPortraitLeft', 'paddingTabletPortraitBottom',
+        'paddingTabletLandscapeTop', 'paddingTabletLandscapeRight', 'paddingTabletLandscapeLeft', 'paddingTabletLandscapeBottom',
+        'paddingDesktopTop', 'paddingDesktopRight', 'paddingDesktopLeft', 'paddingDesktopBottom',
+    ];
+
+    foreach ($padding_attributes as $attribute) {
+        if (array_key_exists($attribute, $attributes) && $attributes[$attribute] != 'inherit') {
+            $sanitized_value = $sanitize_padding($attributes[$attribute]);
+            if (!empty($sanitized_value)) {
+                $class .= " {$attribute}-{$sanitized_value} ";
+            }
+        }
+    }
+
+    return $class;
 }
