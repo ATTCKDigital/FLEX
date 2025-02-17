7f67f13ebd3be0531d61e37848ecf4717334b195
Dennis Plucinik II <dennis@attck.com>
2025-02-17 14:02:23 -0500

    Helper function to sanitize and validate margin values

--- .githooks/gutenberg/components/gb-component_margin/classes.php
+++ .githooks/gutenberg/components/gb-component_margin/classes.php
@@ -3,27 +3,38 @@
 namespace FLEX_LAYOUT_SYSTEM\Components\Margin;
 
 function margin_options_classes( $attributes ) {
-	$class = '';
-	$class .= array_key_exists('marginTop', $attributes) && $attributes['marginTop'] != 'inherit' ? " margin-top-{$attributes['marginTop']} " : '';
-	$class .= array_key_exists('marginRight', $attributes) && $attributes['marginRight'] != 'inherit' ? " margin-right-{$attributes['marginRight']} " : '';
-	$class .= array_key_exists('marginLeft', $attributes) && $attributes['marginLeft'] != 'inherit' ? " margin-left-{$attributes['marginLeft']} " :'';
-	$class .= array_key_exists('marginBottom', $attributes) && $attributes['marginBottom'] != 'inherit' ? " margin-bottom-{$attributes['marginBottom']} " :'';
-	$class .= array_key_exists('marginPhoneTop', $attributes) && $attributes['marginPhoneTop'] != 'inherit' ? " margin-phone-top-{$attributes['marginPhoneTop']} " :'';
-	$class .= array_key_exists('marginPhoneRight', $attributes) && $attributes['marginPhoneRight'] != 'inherit' ? " margin-phone-right-{$attributes['marginPhoneRight']} " :'';
-	$class .= array_key_exists('marginPhoneLeft', $attributes) && $attributes['marginPhoneLeft'] != 'inherit' ? " margin-phone-left-{$attributes['marginPhoneLeft']} " :'';
-	$class .= array_key_exists('marginPhoneBottom', $attributes) && $attributes['marginPhoneBottom'] != 'inherit' ? " margin-phone-bottom-{$attributes['marginPhoneBottom']} " :'';
-	$class .= array_key_exists('marginTabletPortraitTop', $attributes) && $attributes['marginTabletPortraitTop'] != 'inherit' ? " margin-tablet-portrait-top-{$attributes['marginTabletPortraitTop']} " :'';
-	$class .= array_key_exists('marginTabletPortraitRight', $attributes) && $attributes['marginTabletPortraitRight'] != 'inherit' ? " margin-tablet-portrait-right-{$attributes['marginTabletPortraitRight']} " :'';
-	$class .= array_key_exists('marginTabletPortraitLeft', $attributes) && $attributes['marginTabletPortraitLeft'] != 'inherit' ? " margin-tablet-portrait-left-{$attributes['marginTabletPortraitLeft']} " :'';
-	$class .= array_key_exists('marginTabletPortraitBottom', $attributes) && $attributes['marginTabletPortraitBottom'] != 'inherit' ? " margin-tablet-portrait-bottom-{$attributes['marginTabletPortraitBottom']} " :'';
-	$class .= array_key_exists('marginTabletLandscapeTop', $attributes) && $attributes['marginTabletLandscapeTop'] != 'inherit' ? " margin-tablet-landscape-top-{$attributes['marginTabletLandscapeTop']} " :'';
-	$class .= array_key_exists('marginTabletLandscapeRight', $attributes) && $attributes['marginTabletLandscapeRight'] != 'inherit' ? " margin-tablet-landscape-right-{$attributes['marginTabletLandscapeRight']} " :'';
-	$class .= array_key_exists('marginTabletLandscapeLeft', $attributes) && $attributes['marginTabletLandscapeLeft'] != 'inherit' ? " margin-tablet-landscape-left-{$attributes['marginTabletLandscapeLeft']} " :'';
-	$class .= array_key_exists('marginTabletLandscapeBottom', $attributes) && $attributes['marginTabletLandscapeBottom'] != 'inherit' ? " margin-tablet-landscape-bottom-{$attributes['marginTabletLandscapeBottom']} " :'';
-	$class .= array_key_exists('marginDesktopTop', $attributes) && $attributes['marginDesktopTop'] != 'inherit' ? " margin-desktop-top-{$attributes['marginDesktopTop']} " :'';
-	$class .= array_key_exists('marginDesktopRight', $attributes) && $attributes['marginDesktopRight'] != 'inherit' ? " margin-desktop-right-{$attributes['marginDesktopRight']} " :'';
-	$class .= array_key_exists('marginDesktopLeft', $attributes) && $attributes['marginDesktopLeft'] != 'inherit' ? " margin-desktop-left-{$attributes['marginDesktopLeft']} " :'';
-	$class .= array_key_exists('marginDesktopBottom', $attributes) && $attributes['marginDesktopBottom'] != 'inherit' ? " margin-desktop-bottom-{$attributes['marginDesktopBottom']} " :'';
+    $class = '';
 
-	return $class;
+    // Helper function to sanitize and validate margin values
+    $sanitize_margin = function ($value) {
+        if (!is_string($value)) {
+            return ''; // Handle non-string values
+        }
+        $value = sanitize_text_field($value); // Sanitize to prevent XSS
+        $value = str_replace(' ', '', $value); // Remove spaces
+        if (strlen($value) > 20) {
+            return ''; // Limit length to prevent excessively long class names
+        }
+        return $value;
+    };
+
+    // Process each margin attribute, using the helper function
+    $margin_attributes = [
+        'marginTop', 'marginRight', 'marginLeft', 'marginBottom',
+        'marginPhoneTop', 'marginPhoneRight', 'marginPhoneLeft', 'marginPhoneBottom',
+        'marginTabletPortraitTop', 'marginTabletPortraitRight', 'marginTabletPortraitLeft', 'marginTabletPortraitBottom',
+        'marginTabletLandscapeTop', 'marginTabletLandscapeRight', 'marginTabletLandscapeLeft', 'marginTabletLandscapeBottom',
+        'marginDesktopTop', 'marginDesktopRight', 'marginDesktopLeft', 'marginDesktopBottom',
+    ];
+
+    foreach ($margin_attributes as $attribute) {
+        if (array_key_exists($attribute, $attributes) && $attributes[$attribute] != 'inherit') {
+            $sanitized_value = $sanitize_margin($attributes[$attribute]);
+            if (!empty($sanitized_value)) {
+                $class .= " {$attribute}-{$sanitized_value} ";
+            }
+        }
+    }
+
+    return $class;
 }
